c46f98cf4fa78920028a69fcff590ba47559c038
lumbardhgashi <lg52753@ubt-uni.net>
2023-06-19 14:43:36 +0200

    Created Department model and DepartmentEntity

---
+++ RateForProfessor/Models/Department.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RateForProfessor.Models
+{
+    public class Department
+    {
+        [Key] // Annotation for primary key
+        public int DepartmentId { get; set; } // Primary Key -> DepartmentId
+
+        [ForeignKey("University")] // Annotation for foreign key
+        public int UniversityId { get; set; } //Foreing Key -> UniversityId
+
+        public string Name { get; set; }
+        public int EstablishedYear { get; set; }
+        public string Description { get; set; }
+        public int NrStaff { get; set; }
+        public int NrStudent { get; set; }
+        public int NrCourse { get;  set; }  
+
+
+
+       // public virtual University University { get; set; } // Navigation property for University
+
+
+    }
+}
+
+
