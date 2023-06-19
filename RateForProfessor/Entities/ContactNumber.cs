61f6d754d639e7b86395d96c5818833b95349630
Rinor Kadriu <rinorkadriu6@gmail.com>
2023-06-19 15:53:09 +0200

    ContactNumber model and entity added

---
+++ RateForProfessor/Entities/ContactNumber.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RateForProfessor.Models
+{
+    public class ContactNumber
+    {
+        [Key, ForeignKey("University")]
+        public int UniversityId { get; set; }
+
+        [Key, ForeignKey("ContactNumber")]
+        public int ContactNumberId { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        // public virtual University University { get; set; } // Navigation property for University
+    }
+}
