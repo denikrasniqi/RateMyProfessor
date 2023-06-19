f4f2ef430c9f61e0bc6d8289833877ea1b36d210
GentritH <93375355+GentritH@users.noreply.github.com>
2023-06-19 15:31:18 +0200

    Created Student model and entity

---
+++ RateForProfessor/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RateForProfessor.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public int Grade { get; set; }
+
+        //public Gender Gender { get; set; }
+
+        //public Department Department { get; set; }
+    }
+}
