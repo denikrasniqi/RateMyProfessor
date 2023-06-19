d27c70564d61bf7c45db27a3d0ef7ddfa3faf1d8
Rinor Kadriu <rinorkadriu6@gmail.com>
2023-06-19 15:58:22 +0200

    small fix

--- RateForProfessor/Entities/Address.cs
+++ RateForProfessor/Entities/Address.cs
@@ -4,16 +4,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RateForProfessor.Entities
 {
-    [Key]
-    [ForeignKey("University")]
-    public int UniversityId { get; set; }
+    public class Address
+    {
+        [Key, ForeignKey("University")]
+        public int UniversityId { get; set; }
 
-    [Key]
-    [ForeignKey]
-    public int AddressId { get; set; }
+        [Key, ForeignKey("Address")]
+        public int AddressId { get; set; }
 
-    public string Country { get; set; }
-    public string City { get; set; }
-    public string Street { get; set; }
-    public int ZIPCode { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int ZIPCode { get; set; }
+
+        // public virtual University University { get; set; } // Navigation property for University
+    }
 }
--- RateForProfessor/Models/Address.cs
+++ RateForProfessor/Models/Address.cs
@@ -5,17 +5,17 @@ namespace RateForProfessor.Models
 {
     public class Address
     {
-        [Key]
-        [ForeignKey("University")]
+        [Key, ForeignKey("University")]
         public int UniversityId { get; set; }
 
-        [Key]
-        [ForeignKey]
+        [Key, ForeignKey("Address")]
         public int AddressId { get; set; }
 
         public string Country { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
         public int ZIPCode { get; set; }
+
+        // public virtual University University { get; set; } // Navigation property for University
     }
 }
