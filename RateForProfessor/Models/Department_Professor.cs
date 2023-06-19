b34c7820379e6b83312649e3c5e6789737f40dad
lumbardhgashi <lg52753@ubt-uni.net>
2023-06-19 15:17:29 +0200

    Created Department_Professor model and Department_ProfessorEntity

---
+++ RateForProfessor/Models/Department_Professor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RateForProfessor.Models
+{
+    public class Department_Professor
+    {
+        [Key, Column(Order =1)] // 
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; } // Foreing Key -> DepartmentId
+
+        [Key, Column(Order =2)]
+        [ForeignKey("Professor")]
+        public int ProfessorId { get; set; }
+
+        //public virtual Department Department {get; set;} // Represents the associated Department
+        //public virtual Professor Professor {get; set;} // Represents the associated Professor
+    }
+}
