32959be550bede51f6458e838720fcea98ad631f
GashiEtrit <etrit.gashi@hotmail.com>
2023-06-19 15:33:18 +0200

    added ProfessorCourse model and entities

---
+++ RateForProfessor/Models/ProfessorCourse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RateForProfessor.Models
+{
+    public class ProfessorCourse
+    {
+
+        [Key] // Annotation for primary key
+        public int ProfessorCourseId { get; set; } // PK Id
+
+        [ForeignKey("Professor")] // Annotation for foreign key
+        public int ProfessorId { get; set; } // ProfessorID FK
+
+        [ForeignKey("Course")] // Annotation for foreign key
+        public int CourseId { get; set; } // CourseID FK
+    }
+}
+
+
