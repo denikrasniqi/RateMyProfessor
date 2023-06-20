37d03a263c4c11209fec9447a3bba33594a9778b
GashiEtrit <etrit.gashi@hotmail.com>
2023-06-20 23:01:09 +0200

    Refactored code

--- RateForProfessor/Entities/ProfessorEntity.cs
+++ RateForProfessor/Entities/ProfessorEntity.cs
@@ -1,23 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RateForProfessor.Entities
 {
     public class ProfessorEntity
     {
         [Key]
-        public int ProfessorId { get; set; } 
+        public int ProfessorId { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Education { get; set; }
 
+        [Required]
         public string Role { get; set; }
 
     }
--- RateForProfessor/Models/Professor.cs
+++ RateForProfessor/Models/Professor.cs
@@ -1,23 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace RateForProfessor.Models
 {
     public class Professor
     {
-        [Key] 
-        public int ProfessorId { get; set; } 
+        [Key]
 
+        public int ProfessorId { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Education { get; set; }
 
+        [Required]
         public string Role { get; set; }
 
 
