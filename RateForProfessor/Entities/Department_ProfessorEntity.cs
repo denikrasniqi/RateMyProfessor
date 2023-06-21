6b2c90015b310c8759ed8720a19f633d735e8cf5
lumbardhgashi <93322621+lumbardhgashi@users.noreply.github.com>
2023-06-21 10:17:24 +0200

    Update DepartmentProfessorEntity.cs

--- RateForProfessor/Entities/Department_ProfessorEntity.cs
+++ RateForProfessor/Entities/Department_ProfessorEntity.cs
@@ -3,16 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RateForProfessor.Entities
 {
-    public class Department_Professor
+    public class DepartmentProfessorEntity
     {
         [Key, Column(Order = 1)] 
         [ForeignKey("Department")]
         public int DepartmentId { get; set; } // Foreing Key -> DepartmentId
-
         [Key, Column(Order = 2)]
         [ForeignKey("Professor")]
         public int ProfessorId { get; set; }
-
         //public virtual Department Department {get; set;} // Represents the associated Department
         //public virtual Professor Professor {get; set;} // Represents the associated Professor
     }
