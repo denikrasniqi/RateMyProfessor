1ab978a60d3d39147ea6672915879efb81f5eed8
lumbardhgashi <lg52753@ubt-uni.net>
2023-06-21 13:43:50 +0200

    feature/RMP-13: added Database Connection, config CORS, created Context folder with AppDbContext class

---
+++ RateForProfessor/Context/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RateForProfessor.Entities;
+
+namespace RateForProfessor.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AddressEntity> Addresses { get; set; }
+        public DbSet<ContactNumberEntity> ContactNumbers { get; set; }
+        public DbSet<CourseEntity> Courses { get; set; }
+        public DbSet<DepartmentEntity> Departments { get; set; }
+        public DbSet<DepartmentProfessorEntity> DepartmentProfessors { get; set; }
+        public DbSet<ProfessorCourseEntity> ProfessorCourses { get; set; }
+        public DbSet<ProfessorEntity> Profesors { get; set; }
+        public DbSet<RateProfessorEntity> RateProfessors { get; set; }
+        public DbSet<RateUniversityEntity> RateUniversities { get; set; }
+        public DbSet<StudentEntity> Students { get; set; }
+        public DbSet<UniversityEntity> Universities { get; set; }
+        
+        
+        
+        
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- RateForProfessor/Program.cs
+++ RateForProfessor/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using RateForProfessor.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
