261dbd6f8cb3ee17445b3a5543bf559ae4506ba2
lumbardhgashi <93322621+lumbardhgashi@users.noreply.github.com>
2023-06-21 11:06:17 +0200

    Update Department.cs

--- RateForProfessor/Models/Department.cs
+++ RateForProfessor/Models/Department.cs
@@ -6,17 +6,25 @@ namespace RateForProfessor.Models
 {
     public class Department
     {
-        [Key] // Annotation for primary key
-        public int DepartmentId { get; set; } // Primary Key -> DepartmentId
-        [ForeignKey("University")] // Annotation for foreign key
-        public int UniversityId { get; set; } //Foreing Key -> UniversityId
+        [Key] 
+        public int DepartmentId { get; set; }
+        
+        [ForeignKey("University")]
+        public int UniversityId { get; set; }
+        
         public string Name { get; set; }
+        
         public int EstablishedYear { get; set; }
+        
         public string Description { get; set; }
+        
         public int StaffNumber { get; set; }
+        
         public int StudentNumber { get; set; }
+        
         public int CourseNumber { get;  set; }  
-       // public virtual University University { get; set; } // Navigation property for University
+        
+       // public virtual University University { get; set; }
     }
 }
 
