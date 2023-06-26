4369e60097fa1e0811eebb2cb76b89a2101288f6
Rinor Kadriu <rinorkadriu6@gmail.com>
2023-06-26 11:45:06 +0200

    feature-RMP-15 intial commit, folders and controller created

---
+++ RateForProfessor/Controllers/UserRegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateForProfessor.Controllers
+{
+    [ApiController]
+    public class UserRegistrationController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetStudents()
+        {
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetStudent()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult CreateStudent()
+        {
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateStudent()
+        {
+            return null;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteStudent()
+        {
+            return null;
+        }
+    }
+}
