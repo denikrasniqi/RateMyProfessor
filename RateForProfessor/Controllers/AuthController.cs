cff526b39fa937ef8bd048df95af0b36d87eea4f
Denis Krasniqi <denis_me6@hotmail.com>
2023-07-03 15:24:25 +0200

    feature/RMP-17: Added Validator for Login

--- RateForProfessor/Controllers/AuthController.cs
+++ RateForProfessor/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using RateForProfessor.Models;
 using RateForProfessor.Services.Interfaces;
+using RateForProfessor.Validators;
 
 namespace RateForProfessor.Controllers
 {
@@ -16,19 +18,30 @@ namespace RateForProfessor.Controllers
         }
 
         [HttpPost("login")]
-        public IActionResult Login(string email, string password)
+        public IActionResult Login(Student student)
         {
+            LoginValidator validator = new LoginValidator();
+            var validationResult = validator.Validate(student);
+
+            if (!validationResult.IsValid)
+            {
+                foreach (var error in validationResult.Errors)
+                {
+                    ModelState.AddModelError("", error.ErrorMessage);
+                }
+                return BadRequest(ModelState);
+            }
             // Perform authentication
-            var token = _authService.AuthenticateUser(email, password);
-            var message = "User " + email + " Has Logged in!";
+            var token = _authService.AuthenticateUser(student.Email, student.Password);
+            var message = "User " + student.Email + " Has Logged in!";
             if (token == null)
             {
                 return Unauthorized(); // Return 401 Unauthorized if authentication fails
             }
-            else
-            {
-                return Ok(message);
-            }
+            //else
+            //{
+            //    return Ok(message);
+            //}
 
             // Return the token in the response
             return Ok(new { Token = token });
---
+++ RateForProfessor/Validators/LoginValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using RateForProfessor.Models;
+
+namespace RateForProfessor.Validators
+{
+    public class LoginValidator : AbstractValidator<Student>
+    {
+        public LoginValidator()
+        {
+            RuleFor(student => student.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email address.");
+            
+            RuleFor(student => student.Password)
+           .NotEmpty()
+           .WithMessage("Password is required.")
+           .MinimumLength(8).WithMessage("Password must be at least 8 characters long.");
+        }
+
+    }
+}
