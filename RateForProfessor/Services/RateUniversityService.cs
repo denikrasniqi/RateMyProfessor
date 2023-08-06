9224869205ef137296d803829e7bb47a1d52451d
Denis Krasniqi <denis_me6@hotmail.com>
2023-08-06 23:26:04 +0200

    Feature/RMP - Get total ratings for universities

--- RateForProfessor/Services/RateUniversityService.cs
+++ RateForProfessor/Services/RateUniversityService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using RateForProfessor.Entities;
 using RateForProfessor.Models;
+using RateForProfessor.Repositories;
 using RateForProfessor.Repositories.Interfaces;
 using RateForProfessor.Services.Interfaces;
 
@@ -9,12 +10,14 @@ namespace RateForProfessor.Services
     public class RateUniversityService : IRateUniversityService
     {
         public readonly IRateUniversityRepository _rateUniversityRepository;
+        public readonly IUniversityRepository _universityRepository;
         private readonly IMapper _mapper;
 
-        public RateUniversityService(IRateUniversityRepository rateUniversityRepository, IMapper mapper)
+        public RateUniversityService(IRateUniversityRepository rateUniversityRepository, IMapper mapper, IUniversityRepository universityRepository)
         {
             _rateUniversityRepository = rateUniversityRepository;
             _mapper = mapper;
+            _universityRepository = universityRepository;
         }
 
         public RateUniversity CreateRateUniversity(RateUniversity rateUniversity)
@@ -74,6 +77,32 @@ namespace RateForProfessor.Services
             var updatedRateUniversity = _mapper.Map<RateUniversityEntity>(rateUniversity);
 
             _rateUniversityRepository.UpdateRateUniversity(updatedRateUniversity);
+        }
+        public List<UniversityOverallRating> GetOverallRatingUniversities()
+        {
+            var universityRatings = _rateUniversityRepository.GetAllRateUniversity()
+            .Join(
+                    _universityRepository.GetAllUniversites(),
+                    rateUniversity => rateUniversity.UniversityId,
+                    university => university.UniversityId,
+                    (rateUniversity, university) => new UniversityOverallRating
+                    {
+                        UniversityId = rateUniversity.UniversityId,
+                        UniversityName = university.Name,
+                        OverallRating = (int)rateUniversity.Overall
+                    }
+                )
+                .GroupBy(r => r.UniversityId)
+                .Select(g => new UniversityOverallRating
+                {
+                    UniversityId = g.Key,
+                    UniversityName = g.First().UniversityName,
+                    OverallRating = (int)g.Average(r => r.OverallRating),
+                    TotalRatings = g.Count()
+                })
+                .ToList();
+
+            return universityRatings;
         }
     }
 }
